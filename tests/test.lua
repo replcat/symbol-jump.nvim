923e4899a2d49a381615879c955a361eb833d9fa
replcat <replcat@imap.cc>
2025-03-04 20:53:11 +1100

    Attach language server client in test env

--- tests/test.lua
+++ tests/test.lua
@@ -1,10 +1,13 @@
+--- @type Luassert
 local assert = require "luassert"
+
 local nio = require "nio"
 local it = nio.tests.it
 local before_each = nio.tests.before_each
 local after_each = nio.tests.after_each
 
 local fennel_fixture = "./tests/fixtures/fixture.fnl"
+local timeout = vim.env.CI and 1000 or 100
 
 before_each(function()
   assert.equal("", vim.fn.expand "%")
@@ -37,5 +40,17 @@ describe("the test environment", function()
     assert.no_error(function()
       vim.treesitter.get_parser()
     end)
+  end)
+
+  it("attaches the language server client", function()
+    vim.cmd.edit(fennel_fixture)
+    nio.sleep(timeout)
+
+    --- @diagnostic disable-next-line: deprecated
+    local get_clients = vim.lsp.get_clients or vim.lsp.get_active_clients
+    local clients = get_clients { bufnr = 0 }
+
+    assert.equal(1, #clients)
+    assert.equal("fennel-ls", clients[1].name)
   end)
 end)
