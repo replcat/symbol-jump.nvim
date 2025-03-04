bcab33196ee37a855335ac0d0a9217b030f0993d
replcat <replcat@imap.cc>
2025-03-04 20:53:11 +1100

    Attach language server client in test env

--- tests/init.lua
+++ tests/init.lua
@@ -4,3 +4,17 @@ vim.o.rtp = vim.o.rtp .. "," .. ROCKS_TREE .. "/lib/lua/5.1"
 vim.o.modeline = true -- disabled by default in CI
 vim.o.shadafile = "NONE"
 vim.o.swapfile = false
+
+--- @diagnostic disable-next-line: deprecated
+local fennel_ls = assert(vim.lsp.start_client {
+  name = "fennel-ls",
+  cmd = { assert(vim.env.ROCKS_TREE) .. "/bin/fennel-ls" },
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "fennel",
+  callback = function(args)
+    local ok = vim.lsp.buf_attach_client(args.buf, fennel_ls)
+    assert(ok)
+  end,
+})
